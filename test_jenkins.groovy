5e769faa4adbd70523e461084dc9e6b93c16059f
KDONJS <yorlinssj@hotmail.com>
2024-01-01 20:42:45 -0500

    dddd

--- test_jenkins.groovy
+++ test_jenkins.groovy
@@ -14,16 +14,7 @@ pipeline {
             steps { 
 
                 withCredentials([string(credentialsId: '1', variable: 'DB_PASSWORD')]) {
-                    script {
-
-                        def sql = Sql.newInstance("jdbc:mysql://localhost:3306/productosplazavea", "root",  DB_PASSWORD , "com.mysql.jdbc.Driver")
-
-                        def script = readFile('./DDL_CREATE_TABLE.sql')
-
-                        sql.execute(script)
-
-                        sql.close()
-                    }
+                    sh "mysql -u ROOT -p ${DB_PASSWORD} -h localhost productosplazavea < ./DDL_CREATE_TABLE.sql"
                 }
             }
         }
