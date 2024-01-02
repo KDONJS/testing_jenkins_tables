76d6fd9723ab47c064d85c71eeb3a22d25268343
KDONJS <yorlinssj@hotmail.com>
2024-01-01 20:27:13 -0500

    testing

---
+++ DDL_CREATE_TABLE.sql
@@ -0,0 +1,6 @@
+CREATE TABLE empleados (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    edad INT,
+    departamento VARCHAR(100)
+);
--- test_jenkins.groovy
+++ test_jenkins.groovy
@@ -4,7 +4,22 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                echo 'Hello World'
+                echo 'EJECUTANDO DDL'
+            }
+        }
+    
+        stage('Execute SQL Script') {
+            steps {
+                script {
+                    
+                    def sql = Sql.newInstance("jdbc:mysql://localhost:3306/productosplazavea", "root",  DB_PASSWORD , "com.mysql.jdbc.Driver")
+
+                    def script = readFile('./DDL_CREATE_TABLE.sql')
+
+                    sql.execute(script)
+
+                    sql.close()
+                }
             }
         }
     }
